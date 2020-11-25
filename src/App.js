94ad03148a1e5a303832fdf916960ed23cf9e775
ferdinand <ferdinandmwal@gmail.com>
2020-11-25 14:06:08 +0300

    navbar commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import { Route, Switch } from "react-router-dom";
+import Navbar from './components/Navbar'
 
 function App() {
-  return <>hello from app</>;
+  return (
+    <>
+    <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/roooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        //! Error page becomes default if path doesnt exist
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
