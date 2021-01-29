bc0fef2afd45d8810000aa55a60ec7f31ec3ae68
ferdinand <ferdinandmwal@gmail.com>
2021-01-29 13:25:31 +0300

    added name

--- src/components/Room.js
+++ src/components/Room.js
@@ -11,8 +11,9 @@ const {name, slug,images, price } = room;
                 <h6>${price}</h6>
                 <p>per night</p>
             </div>
-            <Link to ={`/rooms/${slug}`} />
+            <Link to ={`/rooms/${slug}`} className="btn-primary room-link">Features</Link>
             </div>
+            <p className="room-info">{name}</p>
         </article>
     )
 }
