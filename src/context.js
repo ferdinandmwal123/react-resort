9a59058e239e50e654f762566bfb3b8e9f1fac77
ferdinand <ferdinandmwal@gmail.com>
2021-02-06 14:58:41 +0300

    getRoom

--- src/context.js
+++ src/context.js
@@ -36,6 +36,10 @@ class RoomProvider extends Component {
     return tempItems;
   }
 
+  getRoom = (slug) =>{
+    
+  }
+
   render() {
     return (
       <RoomContext.Provider value={{ ...this.state }}>
