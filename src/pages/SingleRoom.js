a9d20bd01b881d825ff592e3161fb150adccb79f
ferdinand <ferdinandmwal@gmail.com>
2021-02-16 15:04:54 +0300

    1

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -41,28 +41,37 @@ export default class SingleRoom extends Component {
       pets,
       images,
     } = room;
-    const [mainImg,...defaultImg] = images;
+    const [mainImg, ...defaultImg] = images;
     return (
       <>
-      <StyledHero img={mainImg|| this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
-      <section className="single-room">
-       <div className="single-room-images">
-         {defaultImg.map((item, index) => {
-           return <img key={index} src={item} alt={name} />
-         })}
-         </div>   
-         <div className="single-room-info">
-          <article className="desc">
-            <h3>details</h3>
-          </article>
-         </div>
-      </section>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>size : ${size}SQFT</h6>
+              <h6>max capacity : {
+              capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+            </article>
+          </div>
+        </section>
       </>
     );
   }
