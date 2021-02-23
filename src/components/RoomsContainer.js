b9bad6637960bf04578b8559587a3685efa38bc6
ferdinand <ferdinandmwal@gmail.com>
2021-02-23 16:15:23 +0300

    RoomsContainer

---
+++ src/components/RoomsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RoomsFilter from './RoomsFilter.js';
+import RoomList from './RoomList.js';
+
+
+export default function RoomsContainer() {
+    return (
+        <div>
+          Hello from  RoomsContainer  
+        </div>
+    )
+}
