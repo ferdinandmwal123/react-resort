618b6a51043e071730d2fb3d60e51c3fd7c0ba12
ferdinand <ferdinandmwal@gmail.com>
2021-02-24 15:40:45 +0300

    contextr

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import RoomsFilter from './RoomFilter.js';
-import RoomList from './RoomList.js';
-
+import React from "react";
+import RoomsFilter from "./RoomFilter.js";
+import RoomList from "./RoomList.js";
+import { RoomConsumer } from "../context";
+import Loading from "./Loading";
 
 export default function RoomContainer() {
-    return (
-        <div>
-          Hello from  RoomsContainer  
-          <RoomsFilter />
-          <RoomList />
-        </div>
-    )
+  return (
+    <RoomConsumer>
+      {
+      (value) => {
+        const {}
+        return (
+          <div>
+            Hello from RoomsContainer
+            <RoomsFilter />
+            <RoomList />
+          </div>
+        );
+      }}
+    </RoomConsumer>
+  );
 }
