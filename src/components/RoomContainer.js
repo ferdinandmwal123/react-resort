a68a2f9513626b8e55b1f968705c61309627029a
ferdinand <ferdinandmwal@gmail.com>
2021-02-27 13:57:09 +0300

    context improve

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,25 +1,40 @@
 import React from "react";
 import RoomsFilter from "./RoomFilter.js";
 import RoomList from "./RoomList.js";
-import { RoomConsumer } from "../context";
+import { withRoomConsumer } from "../context";
 import Loading from "./Loading";
 
-export default function RoomContainer() {
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <RoomConsumer>
-      {(value) => {
-        const { loading, sortedRooms, rooms } = value;
-        if (loading) {
-          return <Loading />;
-        }
-        return (
-          <div>
-            Hello from RoomsContainer
-            <RoomsFilter rooms={rooms} />
-            <RoomList rooms={sortedRooms} />
-          </div>
-        );
-      }}
-    </RoomConsumer>
+    <>
+      <RoomsFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </>
   );
 }
+
+export default withRoomConsumer(RoomContainer);
+
+// export default function RoomContainer() {
+//   return (
+//     <RoomConsumer>
+//       {(value) => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello from RoomsContainer
+//             <RoomsFilter rooms={rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
