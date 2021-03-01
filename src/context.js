fbea43af79d893b4c4dee2fb3bf244cb796e26f8
ferdinand <ferdinandmwal@gmail.com>
2021-03-01 15:04:20 +0300

    1

--- src/context.js
+++ src/context.js
@@ -68,7 +68,7 @@ class RoomProvider extends Component {
 
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom, handleChange: this.handleChange }}>
         {this.props.children}
       </RoomContext.Provider>
     );
