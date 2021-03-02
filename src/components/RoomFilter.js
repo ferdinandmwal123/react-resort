b7f7445008fdf58247af6131292695223541e03e
ferdinand <ferdinandmwal@gmail.com>
2021-03-02 14:01:23 +0300

    context

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -3,5 +3,6 @@ import { useContext } from "react";
 import { RoomContext } from "../context";
 import Title from "../components/Title";
 export default function RoomFilter() {
+  const context = useContext(RoomContext);
   return <div>hello room filter</div>;
 }
