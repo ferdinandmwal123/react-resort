2001ee19fc788769af0834738d558fed608bc17f
ferdinand <ferdinandmwal@gmail.com>
2021-03-15 12:44:51 +0300

    filter by breakfast and pets

--- src/context.js
+++ src/context.js
@@ -84,20 +84,30 @@ class RoomProvider extends Component {
     //transform values
     capacity = parseInt(capacity);
     price = parseInt(price);
-    //filter by type
+    //*filter by type
     if (type !== "all") {
       tempRooms = tempRooms.filter((room) => room.type === type);
     }
-    // filter by capacity
+    //* filter by capacity
     if (capacity !== 1) {
       tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
     }
 
-    //filter by price
-    tempRooms = tempRooms.filter(room => room.price <= price);
+    //*filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
 
-    //filter by size
-    tempRooms = tempRooms.filter(room => room.size >=minSize && room.size <= maxSize)
+    //*filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+    //*filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+    //*filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
     //change state
     this.setState({
       sortedRooms: tempRooms,
